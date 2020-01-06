7890e7e27dde15f3ecfefcdc0f85022b110de927
robzim <zimmelman@gmail.com>
2020-01-06 14:38:37 -0500

    better variable names

--- simple-galileo-viewer.py
+++ simple-galileo-viewer.py
@@ -1,39 +1,48 @@
+from typing import List, Any
+
 import fortranformat as ff
 import vpython as vp
-x = [] ; y = [] ; z = [] ; myLabels = []
-myFile = open('fake data.txt','r')
+
+X = []
+Y = []
+Z = []
+My_labels = []
+myFile = open('fake data.txt', 'r')
 myText = myFile.readlines()
-myFirstLineList = myText[0].split(" ",1)
+myFirstLineList = myText[0].split(" ", 1)
 myFortranFormat = myFirstLineList[0]
 myTempString = myFirstLineList[1]
 myConceptCount = int(myTempString[2:4])
 myDimensionCount = int(myTempString[8:10])
-myFirstSplit = myFortranFormat.split('(')
-myConceptsPerLine = int(myFirstSplit[1].split('F')[0])
+my_first_split = myFortranFormat.split('(')
+myConceptsPerLine = int(my_first_split[1].split('F')[0])
 myLinesToSkip = (myDimensionCount % myConceptsPerLine) + 2
 myFortranData = ff.FortranRecordReader(myFortranFormat)
-myLine = 1
+my_line = 1
 myConceptLoopCounter = 1
 while myConceptLoopCounter <= myConceptCount:
-	myTempXYZ = myFortranData.read(myText[myLine])
-	x.append(myTempXYZ[0])
-	y.append(myTempXYZ[1])
-	z.append(myTempXYZ[2])
-	myLine += myLinesToSkip
-	myConceptLoopCounter += 1
+    myTempXYZ = myFortranData.read(myText[my_line])
+    X.append(myTempXYZ[0])
+    Y.append(myTempXYZ[1])
+    Z.append(myTempXYZ[2])
+    my_line += myLinesToSkip
+    myConceptLoopCounter += 1
 myConceptLoopCounter = 1
 while myConceptLoopCounter <= myConceptCount:
-	myLabel = myText[myLine].strip()
-	myLabels.append(myLabel)
-	myLine += 1
-	myConceptLoopCounter += 1
-print("myConceptCount is ",myConceptCount)
-print("Labels are ",myLabels)
-a = vp.canvas(title='Simple Galileo Viewer by Rob Zimmelman c. 2018 All Rights Reserved',width=1400, height=700, background=vp.color.black)
-myBox = vp.box(length=200,width=200,height=0.01,opacity=0.5)
-for i in range(1, myConceptCount):
-	print(i, x[i], y[i], z[i])
-	a = vp.sphere(pos=vp.vector(x[i], y[i], z[i]),color=vp.color.red,radius=1.0)
-	b = vp.label(text=myLabels[i],pos=vp.vector(x[i], y[i], z[i]),color=vp.color.yellow,box=True,line=True,xoffset=75,yoffset=15,radius=12.0)
-	c = vp.arrow(pos=vp.vector(x[i], 0 , z[i]),color=vp.color.blue,shaftwidth=0.25,axis=vp.vector(0,y[i],0))
+    This_label = myText[my_line].strip()
+    My_labels.append(This_label)
+    my_line += 1
+    myConceptLoopCounter += 1
+print("myConceptCount is ", myConceptCount)
+print("Labels are ", My_labels)
+my_sphere = vp.canvas(title='Simple Galileo Viewer by Rob Zimmelman c. 2018 All Rights Reserved',
+					  width=1400, height=700, background=vp.color.black)
 
+myBox = vp.box(length=200, width=200, height=0.01, opacity=0.5)
+for i in range(1, myConceptCount):
+    print(i, X[i], Y[i], Z[i])
+    my_sphere = vp.sphere(pos=vp.vector(X[i], Y[i], Z[i]), color=vp.color.red, radius=1.0)
+    my_label = vp.label(text=My_labels[i], pos=vp.vector(X[i], Y[i], Z[i]), color=vp.color.yellow, box=True, \
+						line=True, xoffset=75, yoffset=15, radius=12.0)
+    my_arrow = vp.arrow(pos=vp.vector(X[i], 0, Z[i]), color=vp.color.blue, shaftwidth=0.25, axis=vp.vector(0, Y[i], 0))
+    print(f'a={my_sphere}, b={my_label}, c={my_arrow}')
